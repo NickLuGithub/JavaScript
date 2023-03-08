896c7c104688c578dbee974fbec22193e41d5bd1
NickLu <word4205@gmail.com>
2023-03-08 16:43:27 +0800

    new CreateTable

--- Object/ObjectCreate.js
+++ Object/ObjectCreate.js
@@ -1,9 +1,21 @@
 let person = {
-    neme: "Kenve",
+    name: "Kenve",
     age: 35,
     height: 180,
     weight: 80
 }
+
+console.log(person.name);
+console.log(person['name']);
+
+let keys = Object.keys(person);
+let values = Object.values(person);
+
+console.log(keys, values)
+
+keys.forEach((keys) => {
+    console.log(`${keys} => ${person[keys]}`);
+})
 
 // %o 可以列印出物件
 console.log(person);
@@ -19,6 +31,7 @@ console.log(p)
 console.log(typeof p)
 
 // 字串 VS 文字
+// Runtime
 
 // 通常跨越網路的時候就要轉 String
 // { neme: 'Kenve', age: 35, height: 180, weight: 80 }
@@ -26,3 +39,6 @@ console.log(typeof p)
 // The OBJ is: { neme: 'Kenve', age: 35, height: 180, weight: 80 }
 // {"neme":"Kenve","age":35,"height":180,"weight":80}
 // string
+
+// 用陣列包物件比較好
+// 因為陣列比較好用
