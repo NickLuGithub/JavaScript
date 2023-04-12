c4384c83c5408151ec954eb11946ff9379811cf8
NickLu <word4205@gmail.com>
2023-04-12 13:51:34 +0800

    new file

--- modules/helpers.js
+++ modules/helpers.js
@@ -8,4 +8,16 @@ function $g(selector) {
     return nodeList.length == 1 ? nodeList[0] : nodeList;
 }
 
-export { $g }
+
+//Create Element
+function $ce(element, text) {
+    let el = document.createElement(element);
+    //判斷text參數是否合規?
+    if (typeof(text) != "undefined" && text != "" && text != null) {
+        el.innerHTML = text.trim();
+    }
+
+    return el;
+}
+
+export { $g, $ce}
